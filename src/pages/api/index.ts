c96cf87cc39d07ce31194eb301ce2a8a975342e0
Brad Thompson <bthompson6225@gmail.com>
2023-07-03 15:46:26 -0700

    init(environment) add test schema and routing + styles

---
+++ src/pages/api/index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function fetchData() {
+  try {
+    const categories = await prisma.categories.findMany();
+    const products = await prisma.products.findMany();
+    const posts = await prisma.post.findMany();
+    const profiles = await prisma.profile.findMany();
+    const users = await prisma.user.findMany();
+
+    return {
+      categories,
+      products,
+      posts,
+      profiles,
+      users,
+    };
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    await prisma.$disconnect();
+  }
+}
